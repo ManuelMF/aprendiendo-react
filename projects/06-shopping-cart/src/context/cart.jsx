4482520fc726b39087e69a88a07358a9ae98a3df
Manuel Muñoz <mmf19500@gmail.com>
2024-02-19 21:05:21 +0100

    imprive arquitecture

--- projects/06-shopping-cart/src/context/cart.jsx
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -1,35 +1,39 @@
-import { createContext, useState } from 'react'
-
+import { createContext, useReducer, useState } from 'react'
+import { cartInitialState, cartReducer } from '../reducers/cart'
 // crear contexto
 export const CartContext = createContext()
 
+function useCartReducer() {
+  const [state, dispatch] = useReducer(cartReducer, cartInitialState)
+
+  const addToCart = (product) =>
+    dispatch({
+      type: 'ADD_TO_CART',
+      payload: product,
+    })
+
+  const removeFromCart = (product) =>
+    dispatch({
+      type: 'REMOVE_FROM_CART',
+      payload: product,
+    })
+
+  const cleanCart = () =>
+    dispatch({
+      type: 'CLEAN_CART',
+    })
+
+  return { state, addToCart, removeFromCart, cleanCart }
+}
+
 // crear provider
+// la dependencia de user React Context
+// es Mínima
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState([])
-
-  const addToCart = (product) => {
-    const productInCartIndex = cart.findIndex((item) => item.id === product.id)
-
-    if (productInCartIndex >= 0) {
-      const newCart = structuredClone(cart) // crea copia exacta de los arr
-      newCart[productInCartIndex].quantity += 1
-      return setCart(newCart)
-    }
-
-    setCart((prevState) => [...prevState, { ...product, quantity: 1 }])
-  }
-
-  const cleanCart = () => {
-    setCart([])
-  }
-
-  const RemoveFromCart = (product) => {
-    setCart((prevState) => prevState.filter((item) => item.id !== product.id))
-  }
-
+  const { state, addToCart, removeFromCart, cleanCart } = useCartReducer()
   return (
     <CartContext.Provider
-      value={{ cart, addToCart, cleanCart, RemoveFromCart }}
+      value={{ cart: state, addToCart, cleanCart, removeFromCart }}
     >
       {children}
     </CartContext.Provider>
--- projects/06-shopping-cart/src/hooks/useCart.js
+++ projects/06-shopping-cart/src/hooks/useCart.js
@@ -2,10 +2,10 @@ import { useContext } from 'react'
 import { CartContext } from '../context/cart'
 
 export function useCart() {
-  const { cart, addToCart, cleanCart, RemoveFromCart } = useContext(CartContext)
+  const { cart, addToCart, cleanCart, removeFromCart } = useContext(CartContext)
 
   const checkProductInCart = (product) => {
     return cart.some((item) => item.id === product.id)
   }
-  return { cart, addToCart, cleanCart, checkProductInCart, RemoveFromCart }
+  return { cart, addToCart, cleanCart, checkProductInCart, removeFromCart }
 }
