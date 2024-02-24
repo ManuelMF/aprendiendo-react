fa5ac7974128b2b0b4dd3ff769d8b6fbf47f4534
Manuel Muñoz <mmf19500@gmail.com>
2024-02-24 14:25:01 +0100

    Lazy load of the routes

--- projects/07-react-router/src/App.jsx
+++ projects/07-react-router/src/App.jsx
@@ -1,11 +1,12 @@
+import { Suspense, lazy } from 'react' // importar de forma dinamica para que no haya problemas de rendimiento
 import './App.css'
-import HomePage from './pages/Home.jsx'
-import AboutPage from './pages/About.jsx'
-import SearchPage from './pages/Search.jsx'
+const HomePage = lazy(() => import('./pages/Home.jsx'))
+const AboutPage = lazy(() => import('./pages/About.jsx')) // import dinamico
+const SearchPage = lazy(() => import('./pages/Search.jsx'))
 
-import { Router } from './Router.jsx'
+import { Router } from './Router.jsx' // import estático
 import { Route } from './Route.jsx'
-
+// para los imports dinamicos tenemos que envolver el elemento con un Suspense ya que esta en estado suspendido
 const appRoutes = [
   {
     path: '/search/:query',
@@ -16,10 +17,12 @@ const appRoutes = [
 function App() {
   return (
     <main>
-      <Router routes={appRoutes}>
-        <Route path="/" Component={HomePage} />
-        <Route path="/about" Component={AboutPage} />
-      </Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router routes={appRoutes}>
+          <Route path="/" Component={HomePage} />
+          <Route path="/about" Component={AboutPage} />
+        </Router>
+      </Suspense>
     </main>
   )
 }
