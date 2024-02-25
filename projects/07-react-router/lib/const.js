a4dd1e9e7b2b5e9a9e42f12652aa53f19a27c9dc
Manuel Muñoz <mmf19500@gmail.com>
2024-02-25 20:41:25 +0100

    create the prepare and push to npm

---
+++ projects/07-react-router/lib/const.js
@@ -0,0 +1,19 @@
+export const EVENTS = {
+  PUSHSTATE: 'pushstate',
+  POPSTATE: 'popstate',
+}
+
+export const I18N = {
+  es: {
+    title: 'Sobre nosotros',
+    button: 'Ir a la home',
+    description:
+      '¡Hola! Me llamo Manuel y estoy creando un clon de React Router.',
+  },
+  en: {
+    title: 'About us',
+    button: 'Go to the home',
+    description:
+      'Hello! My name is Manuel and I am creating a clone of React Router.',
+  },
+}
